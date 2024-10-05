bc2ff470eba8087bea3e748629bc7fc1fbb9cad0
monocodes <code@wandering-mono.top>
2024-10-06 01:17:10 +0200

    Data Types

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////
+// Values and Variables
+
+// 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+// 2. Log their values to the console.
+let country = 'Germany';
+let continent = 'Europe';
+let population = 84;
+console.log(
+  `Currently I'm in ${country}, it's in ${continent}, it's population is
+  ${population} million people`
+);
+
+/////////////////////////////////////////////////
+// Data Types
+
+// 1. Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+// 2. Log the types of isIsland, population, country and language to the console.
+
+let isIsland = false;
+let language;
+
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
