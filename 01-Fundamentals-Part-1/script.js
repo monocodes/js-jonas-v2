aabb4a9868e8e2ea4190410791162b71ad787a2d
monocodes <code@wandering-mono.top>
2024-10-05 14:19:35 +0200

    S2 - JavaScript Fundamentals - Part 1 - Values and Variables

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,37 @@
+/////////////////////////////////////////////////
+// Linking a JavaScript File
+/* 
+let js = 'amazing';
+if (js == 'amazing') alert('JavaScript is FUN!');
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+ */
+
+/////////////////////////////////////////////////
+// Values and Variables
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
