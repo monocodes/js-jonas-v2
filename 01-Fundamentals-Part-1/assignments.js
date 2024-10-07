6788e5325996f824fe4c6a52754ab4e07645ceef
monocodes <code@wandering-mono.top>
2024-10-08 01:03:33 +0200

    Equality Operators: == vs. ===

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -105,7 +105,11 @@ console.log(description);
 
 /////////////////////////////////////////////////
 // Taking Decisions: if / else Statements
-/* 
+/*
+// 1. If your country's population is greater than 33 million, log a string like this to the console: "Portugal's population is 22 million below average" (the 22 is the average of 33 minus the country's population).
+
+// 2; After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original.
+
 const language = 'german';
 const isIsland = false;
 const country = 'Germany';
@@ -127,3 +131,37 @@ if (population > 33) {
     } million below average`
   );
  */
+
+/////////////////////////////////////////////////
+// Type Conversion and Coercion
+/* 
+// 1. Predict the result of these 5 operations without executing them:
+
+// 2. Execute the operations to check if you were right.
+
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> '617'
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
+ */
+
+/////////////////////////////////////////////////
+// Equality Operators: == vs. ===
+
+// 1. Declare a variable numNeighbours based on a prompt input like this:
+// prompt('How many neighbour countries does your contry have?');
+
+// 2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality == for now).
+
+// 3. Use an else-if block to log 'More than 1 border' in case numNeighbours is greater than 1.
+
+// 4. Use an else block to log 'No borders' (this block will be executed when numNeighbours is 0 or any other value).
+
+// 5. Test the code with different values of numNeighbours, including 1 and 0.
+
+// 6. Change == to ===, and test the code again, with the same values of numNeighbours. Notice what happens when there is exactly 1 border! Why is this happening?
+
+// 7. Finally, convert numNeighbours to a number, and watch what happens now when you input 1.
+
+// 8. Reflect on why we should use the === operator and type conversion in this situation.
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -208,3 +208,51 @@ console.log(n);
 
 /////////////////////////////////////////////////
 // Truthy and Falsy Values
+/* 
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else console.log('You should get a job!');
+
+let height = 0;
+if (height) {
+  console.log('YAY! Height is defined');
+} else console.log('Height is UNDEFINED');
+ */
+
+/////////////////////////////////////////////////
+// Equality Operators: == vs. ===
+/* 
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
+
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+  // '23' == 23
+  console.log('Cool! 23 is an amazing number');
+} else if (favorite === 7) {
+  console.log('7 is also a cool number');
+} else if (favorite === 9) {
+  console.log('9 is also a cool number');
+} else {
+  console.log('Number is not 23 or 7 or 9');
+}
+
+if (favorite !== 23) console.log('Why not 23?');
+ */
+
+/////////////////////////////////////////////////
+// Boolean Logic
