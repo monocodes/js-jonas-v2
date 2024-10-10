0d79edf3f8a506c69fd2fa7084fdfbd4f529da0b
monocodes <code@wandering-mono.top>
2024-10-11 01:12:38 +0200

    Start JavaScript Fundamentals - part 2 - Functions

---
+++ 02-Fundamentals-Part-2/assignments.js
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////
+// Functions
+
+'use strict';
+/* 
+// Write a function called describeCountry which takes three parameters: country, population and capitalCity. Based on this input, the function returns a string with this format: 'Finland has 6 million people and its capital city is Helsinki'.
+
+// Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console.
+
+function describeCountry(country, population, capitalCity) {
+  const desc = `${country} has ${population} million people and its capital is ${capitalCity}`;
+  return desc;
+}
+
+const describedGermany = describeCountry('Germany', 84, 'Berlin');
+console.log(describedGermany);
+
+const describedJapan = describeCountry('Japan', 125, 'Tokyo');
+console.log(describedJapan);
+
+const describedSpain = describeCountry('Spain', 47, 'Madrid');
+console.log(describedSpain);
+ */
