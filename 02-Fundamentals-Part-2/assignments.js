3f31099b3d1644d79c66345211399b74b714346f
monocodes <code@wandering-mono.top>
2024-10-21 23:10:55 +0200

    Section 3 done - Challenge #4

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -358,4 +358,25 @@ for (let i = 0; i < listOfNeighbours.length; i++) {
  */
 
 /////////////////////////////////////////////////
-//
+// The while Loop
+/* 
+// 1. Recreate the challenge from the lecture Looping Arrays, Breaking and Continuing, but this time using a while loop (call the array percentages3).
+
+// 2. Reflect on what solution you like better for this task: the for loop or the while loop?
+
+const populations = [84, 123, 47, 5];
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percentages3 = [];
+
+let i = 0;
+while (i < populations.length) {
+  percentages3.push(percentageOfWorld1(populations[i]));
+  i++;
+}
+
+console.log(percentages3);
+ */
