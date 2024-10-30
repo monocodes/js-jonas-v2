36e279226dc0090ab914530dd4684c3fa5ba1b6e
monocodes <code@wandering-mono.top>
2024-10-30 22:24:36 +0100

    Challenge #1

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -14,8 +14,7 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
  */
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -29,6 +28,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -56,4 +56,28 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+/////////////////////////////////////////////////
+// Coding Challenge #1
+
+// Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+// 1. Select the element with the 'again' class and attach a click event handler
+// 2. In the handler function, restore initial values of the score and secretNumber variables
+// 3. Restore the initial conditions of the message, number, score and guess input field
+// 4. Also restore the original background color (#222) and number width (15rem)
+
+// GOOD LUCK 😀
+
+// Again! button
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
 });
