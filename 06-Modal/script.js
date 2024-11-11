1aeaaf5fda5150a62bf9211fda22819783876d8e
monocodes <code@wandering-mono.top>
2024-11-11 22:51:41 +0100

    Handling an "Esc" Keypress Event - PROJECT #2: Modal Windows completed

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -9,7 +9,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -24,3 +23,12 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // Function for working with object to find out what key was pressed
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
