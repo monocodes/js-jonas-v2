23b33d52ee6c97080e95731e4fa18862816bf0ee
monocodes <monocodes@icloud.com>
2024-12-18 22:19:06 +0100

    Regular Function vs. Arrow Functions

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -139,3 +139,57 @@ f();
 
 /////////////////////////////////////////////////
 // Regular Functions vs. Arrow Functions
+/* 
+var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // Pre ES6 solution: to preserve this keyword for function inside method inside object we can use self/this variable
+    // const self = this; // self or that
+    // const isMillennial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    //   // console.log(this.year >= 1981 && this.year <= 1996);
+    // };
+    // Solution 2
+    // Current solution: to preserve this keyword for function inside method inside object we can use arrow function because they get this keyword from lexical scope
+    const isMillennial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+    isMillennial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  // Arrow functions don't have arguments keyword
+  // console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
+ */
+
+/////////////////////////////////////////////////
+// Memory Management: Primitives vs. Objects
