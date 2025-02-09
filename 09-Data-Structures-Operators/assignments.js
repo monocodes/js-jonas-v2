e0423383102a9125705addfe2d0c02193abb8c0e
monocodes <97431122+monocodes@users.noreply.github.com>
2025-02-10 00:41:23 +0100

    Short Circuiting (&& and ||)

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -387,4 +387,53 @@ printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
  */
 
 /////////////////////////////////////////////////
+// Short Circuiting (&& and ||)
+/* 
+// 5.1
+// Some of the book objects have the programmingLanguage property, which specifies what programming language is used in the book, for example
+
+// {
+//   title: 'Algorithms',
+//   author: ['Robert Sedgewick', 'Kevin Wayne'],
+//   ...
+//   programmingLanguage: 'Java',     // <-- HERE
+// }
+
+// Write a function called hasExamplesInJava that takes a book object from the books array as an argument. This function should return true if the book uses Java, or a string 'no data available' if it uses other language or no programming language at all.
+// Use short-circuiting.
+
+// Example 1
+// hasExamplesInJava(books[0]);
+// Expected output:
+// true
+
+// Example 2
+// hasExamplesInJava(books[1]);
+// Expected output:
+// "no data available"
+
+const hasExamplesInJava = function (book) {
+  // console.log(book.programmingLanguage);
+  console.log(book.programmingLanguage === 'Java' || 'no data available');
+};
+
+hasExamplesInJava(books[0]);
+hasExamplesInJava(books[1]);
+
+// 5.2
+// Some of the book objects have the onlineContent property, which is either true or false. Loop over the books array, and for the books that provide online content, log to the console a string in this format: "${title}" provides online content. Use short-circuiting.
+
+// {
+//   title: 'Operating System Concepts',
+//   // ... removed for clarity
+//   onlineContent: false,          // <-- HERE
+// },
+
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent &&
+    console.log(`"${books[i].title}" provides online content`);
+}
+ */
+
+/////////////////////////////////////////////////
 //
