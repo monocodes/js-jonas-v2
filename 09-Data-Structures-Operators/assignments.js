9dfcc4bbeaea5de4b07b65f849fc2a16329da9fe
monocodes <97431122+monocodes@users.noreply.github.com>
2025-05-21 20:20:14 +0200

    Looping Arrays: The for-of Loop

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -471,4 +471,54 @@ for (let i = 0; i < books.length; i++) {
  */
 
 /////////////////////////////////////////////////
+// Looping Arrays: The for-of Loop
+/* 
+// 8.1
+// Use the for-of loop to loop over the books array and sum the pages of all books. Use the pageSum variable below, and the pages property of the book objects.
+
+let pageSum = 0;
+
+for (const book of books) {
+  pageSum += book.pages;
+}
+console.log(pageSum);
+
+// 8.2
+// Below is the allAuthors variable which stores an empty array. Use the for-of loop to fill allAuthors with the authors of each book from the books array.
+
+// Remember that each book object has the author property, which can be a string (if there is only a single author) or an array (if there are multiple authors). You may need to use the typeof operator. You can also use multiple loops if needed. The allAuthors array should have just one level (no nested arrays).
+
+const allAuthors = [];
+
+for (const book of books) {
+  if (typeof book.author === 'string') {
+    allAuthors.push(book.author);
+  } else {
+    for (const author of book.author) {
+      allAuthors.push(author);
+    }
+  }
+}
+console.log(allAuthors);
+
+// 8.3
+// Use the for-of loop together with Array's entries() method to log each author from allAuthors to the console together with its index. Make the index start from 1, instead of 0.
+
+// Expected output
+// 1. Robert Sedgewick
+// 2. Kevin Wayne
+// 3. Harold Abelson
+//    ...                    // part removed for clarity
+// 15. Cal Newport
+
+for (const [i, el] of allAuthors.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+for (const element of allAuthors.entries()) {
+  console.log(element);
+}
+ */
+
+/////////////////////////////////////////////////
 //
