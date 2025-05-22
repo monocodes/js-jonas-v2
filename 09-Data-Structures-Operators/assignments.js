48eab9cb5eda9dca088166e63c776735a2f419ab
monocodes <97431122+monocodes@users.noreply.github.com>
2025-05-22 20:28:44 +0200

    Optional Chaining (?.)

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -521,4 +521,72 @@ for (const element of allAuthors.entries()) {
  */
 
 /////////////////////////////////////////////////
+// Enhanced Object Literals
+/* 
+// 9.1
+// Below is the bookData array that contains other arrays. Each inner array consists of the property name (first element), and the value (second element). For example, in ['title', 'Computer Networking: A Top-Down Approach'], 'title' is the property name, and 'Computer Networking: A Top-Down Approach' is meant to be the value assigned to that property name.
+// Using computed properties, fill the newBook object with the properties and values from the bookData array. The first one is done already.
+
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+console.log(newBook);
+
+// 9.2
+// Below is the pages variable. Add it as a property of the newBook2 object. Use the shorter way.
+
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages,
+};
+console.log(newBook2);
+ */
+
+/////////////////////////////////////////////////
+// Optional Chaining (?.)
+/* 
+// 10.1
+// Write a function called getFirstKeyword that takes the book object as an argument. This function should return the first keyword from the book's keywords property (array) or undefined (if the keywords property doesn't exist). It shouldn't throw an error. Use optional chaining for that.
+
+// Example 1
+// getFirstKeyword(book[0]);
+
+// Expected output:
+// "computer science"
+
+// Example 2
+// getFirstKeyword(newBook2); // from previous tasks
+
+// Expected output:
+// undefined
+
+const getFirstKeyword = function (book) {
+  return book.keywords?.[0];
+};
+console.log(getFirstKeyword(books[0]));
+
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages,
+};
+
+console.log(getFirstKeyword(newBook2));
+ */
+
+/////////////////////////////////////////////////
 //
